3bee03b430d0e182828cd55aeb7e3d4601659a7d
Matheos Romain <romain.matheos.31@gmail.com>
2021-10-10 22:17:48 +0200

    Correction du problème lié aux espaces. Maintenant c'est bien une lettre sur deux en majuscule et non un carractère sur deux. Ajout d'une condition pour gestion d'erreur à perfectionner.

--- Majuscule.js
+++ Majuscule.js
@@ -1,13 +1,24 @@
 #! node
+/* Afficher une lettre sur deux en majuscule. */
+
+if (typeof process.argv[2] !== "string") {
+  console.log("Veuillez indiquer au script, un string en argument");
+}
+console.log(typeof process.argv[2]);
 
 let argument = process.argv[2];
 let sent = "";
+let space = 0;
 
 for(i = 0; i < argument.length; i++) {
-  if (i % 2 === 0) {
-  sent += process.argv[2][i].toUpperCase();
-  } else {
-  sent += process.argv[2][i].toLowerCase();
+  let num = i + space;
+  if (process.argv[2][i] === " ") {
+  space += 1;
   }
+  if (num % 2 === 0) {
+  sent += process.argv[2][i].toUpperCase();
+} else {
+  sent += process.argv[2][i].toLowerCase();
+}
 }
 console.log(sent);
