4c9bcb945362c324c2a4a3d2c9e84f20ffe211dc
Matheos Romain <romain.matheos.31@gmail.com>
2021-10-17 17:24:02 +0200

    Maj gestion d'erreur. Lorsque rien ou autre chose qu'un string est indiqué en argument, un message d'aide apparait.

--- Majuscule.js
+++ Majuscule.js
@@ -1,28 +1,44 @@
 #! node
 /* Afficher une lettre sur deux en majuscule. */
 
-let argument = process.argv[2];
+// Permet de créer un tableau de string et number
+
+process.argv.shift(process.argv[0]); process.argv.shift(process.argv[1]);
+
+let argString = process.argv;
+let argNumber = [];
+let argNumStg = [];
+
+for (i = 0; i < argString.length; i++) {
+  argNumber[i] = parseInt(argString[i]);
+}
+for (j = 0; j < argNumber.length; j++) {
+  if (isNaN(argNumber[j])) {
+    argNumStg[j] = argString[j];
+  } else {
+    argNumStg[j] = argNumber[j];
+  }
+}
+// Fin création tableau
+
+let argument = argNumStg[0];
 let sent = "";
 let space = 0;
 
-parseInt(argument);
 
 if (typeof argument !== 'string') {
   console.log("Veuillez indiquer au script, un string en argument");
 } else {
   for(i = 0; i < argument.length; i++) {
   let num = i + space;
-  if (process.argv[2][i] === " ") {
+  if (argNumStg[0][i] === " ") {
   space += 1;
   }
   if (num % 2 === 0) {
-  sent += process.argv[2][i].toUpperCase();
+  sent += argNumStg[0][i].toUpperCase();
 } else {
-  sent += process.argv[2][i].toLowerCase();
+  sent += argNumStg[0][i].toLowerCase();
 }
 }
 console.log(sent);
-
 }
-
-console.log(typeof argument);
